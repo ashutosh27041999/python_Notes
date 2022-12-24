acee580fe914dc8841ceca30aa0298352b5e0a66
Ashutosh <ashutoshsrivastava0004@gmail.com>
2022-12-24 10:08:20 +0530

    assigment 3,4,5

---
+++ assigment_2.py
@@ -0,0 +1,37 @@
+#. Write a python script to add comments and print “Learning Python” on screen
+2. Write a python script to add multi line comments and print values of four variables,
+each in a new line. Variable contains any values.
+3. Write a python script to print types of variables. Create 5 variables each of them
+containing different types of data. (like 35, True, “MySirG”,5.46, 3+4j, etc)
+4. Write a python script to print the id of two variables containing the same integer
+values.
+5. Create four variables in a Python script and assign values of different data types to
+them. Write a Python script to print value, its type and id of each variable
+6. Write a python script to print all the keywords
+7. On Python shell use help() function and display the list of keywords
+8. Create two Python files A0.py and A1.py. Create a variable in A1.py and assign some
+value to it. Write a python script to import A1 module in A0 and print value of the
+variable created in A0.py
+9. Name the keywords, used as data in the Python script.
+10. Write a python script to display the current date and time. First create variables to
+store date and time, then display date and time in proper format (like: 13-8-2022 and
+9:00 PM)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ assigment_4.py
@@ -0,0 +1,31 @@
+#1. Write a command to get the Python version you are using.
+print(python--version)
+
+#2. Write a python script to print Hello Python on the screen directly in the command
+#print("Hello python")
+3. Write a python script and store it in a file and execute the file from the command line.
+#4. Write a python script to print Hello Python on the screen using visual studio code.
+# print("hello python")
+#5. Write a python script to print Hello on the first line and Python on the second line
+#print("Hello")
+#6. Write a python script to print “MySirG” on the screen
+#print("My sirG")
+#7. Write a python script to print “Teacher’s Day” on the screen
+#print("Teacher Day")
+#8. Write a python script to print the value of a variable. Variable contains your name as
+#data.
+a=5
+b=6
+print(a,b)
+#9. Write a python script to create variables to store your name, age, qualification, years
+#of experience and print it.
+a="ashutosh"
+print(a)
+
+
+#10. Write a python script to print values of three variables, each in a new line. Variable
+#contains integer values
+a=4
+b=5
+c=6
+print(a,b,c)
